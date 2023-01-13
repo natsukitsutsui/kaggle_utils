fee133fa3d5abee84df7084be7bb707dcc401545
Natsuki Tsutsui <natsuki.tsutsui.08@gmail.com>
2023-01-13 23:51:25 +0900

    modify: load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -17,27 +17,27 @@ def load_data(id_=ID, target_col=TARGET_COL, path_train=DATA_TRAIN_PATH, path_te
     ----------
     train: DataFrame
         訓練データ(idと目的変数を除いたもの)
-    train_labels: ndarray
+    train_labels: DataFrame
         訓練データの目的変数
-    train_ids: ndarray
+    train_ids: DataFrame
         訓練データのid
     features: list
         trainの列一覧
     test: DataFrame
         テストデータ(提出する用のデータ)
-    test_ids: ndarray
+    test_ids: DataFrame
         テストデータのid(最後に予測結果とconcatして提出)
     """
     train_loader = pd.read_csv(path_train)
     train = train_loader.drop([target_col, id_], axis=1)
     features = train.columns.tolist()
     print("\n 訓練データの大きさ:", train.shape)
-    train_labels = train_loader[target_col].values
-    train_ids = train_loader[id_].values
+    train_labels = train_loader[target_col]
+    train_ids = train_loader[id_]
 
     test_loader = pd.read_csv(path_test)
     test = test_loader[features]
     print(" テストデータの大きさ:", test.shape)
-    test_ids = test_loader[id_].values
+    test_ids = test_loader[id_]
 
     return train, train_labels, train_ids, features, test, test_ids
